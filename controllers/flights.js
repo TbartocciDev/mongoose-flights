4f25234bff72e099461f12735cfcde9effac504e
TbartocciDev <tommybartocci@gmail.com>
2023-04-04 23:23:01 -0400

    lesson part 1

---
+++ controllers/flights.js
@@ -0,0 +1,30 @@
+const Flight = require('../models/flight')
+
+
+module.exports = {
+    index,
+    new: newFlight,
+    create
+}
+
+async function index(req,res) {
+    const flights = await Flight.find({})
+    res.render('flights/index', { title: 'Flight List', flights})
+}
+
+async function newFlight(req,res) {
+    res.render('flights/new', {title: 'New Flight Creation'})
+}
+
+async function create(req,res) {
+    for (let key in req.body) {
+        if (req.body[key] === '') delete req.body[key]
+    }
+    try {
+        const flight = await Flight.create(req.body)
+        res.redirect('/flights')
+    } catch (err){
+        console.log(err)
+        res.render('flights/new')
+    }
+}
