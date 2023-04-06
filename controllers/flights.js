74785e68d7b442d31fa7dba6ba785bd7ac6f7ebd
TbartocciDev <tommybartocci@gmail.com>
2023-04-05 21:18:09 -0400

    part 2 completed

--- controllers/flights.js
+++ controllers/flights.js
@@ -4,7 +4,8 @@ const Flight = require('../models/flight')
 module.exports = {
     index,
     new: newFlight,
-    create
+    create,
+    show
 }
 
 async function index(req,res) {
@@ -27,4 +28,9 @@ async function create(req,res) {
         console.log(err)
         res.render('flights/new')
     }
+}
+
+async function show(req,res) {
+    const flight = await Flight.findById(req.params.id)
+    res.render('flights/show', {title: 'Flight Detail', flight})
 }
